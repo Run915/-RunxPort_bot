e52acee1fd92a5517d9fd065d66567f4efb47ed6
Run915 <wjy179179@gmail.com>
2025-05-18 13:45:42 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,8 @@ if __name__ == "__main__":
     async def run():
         await bot_app.initialize()
         await bot_app.start()
-        app.run(port=10000)
+        port = int(os.environ.get("PORT", 10000))
+        app.run(host="0.0.0.0", port=port)
 
     asyncio.run(run())
 
