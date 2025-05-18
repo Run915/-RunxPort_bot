8d5354a194e2821b41a3f8f73927e69ad9535233
Run915 <wjy179179@gmail.com>
2025-05-18 12:29:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
 TOKEN = os.getenv('TOKEN')
-WELCOME_MESSAGE = os.getenv('WELCOME_MESSAGE', 'Hello!')
+WELCOME_MESSAGE = os.getenv('WELCOME_MESSAGE', '👋 各位蒞臨潤匯港的貴賓你好！\n有任何匯率相關的問題，請私訊我，我會盡快為您服務！')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(WELCOME_MESSAGE)
